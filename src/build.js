89ae383824255f337bca0599bd4359d4e39fc25e
Damien Duhamel <d.damien@live.fr>
2017-12-29 19:16:00 +0100

    Change thumbnail width

--- src/build.js
+++ src/build.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 config.postBuild = () => {
 	const images = Utils.getFilesPath(`${config.output}/assets/images/uploads`);
 	fs.mkdirSync(`${config.output}/assets/images/thumbnails`);
-	images.forEach( image => sharp(image).resize(200).toFile(image.replace('uploads', 'thumbnails')) );
+	images.forEach( image => sharp(image).resize(400).toFile(image.replace('uploads', 'thumbnails')) );
 };
 
 builder.build(config);
