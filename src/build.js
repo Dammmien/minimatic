7b99ac53abf0a53a0b7b0e301019d7ad73441d58
Damien Duhamel <d.damien@live.fr>
2020-03-09 20:58:43 +0100

    Change images path

--- src/build.js
+++ src/build.js
@@ -1,11 +1,9 @@
 const project = require('../project.json');
 const Utils = require('./Utils');
 const Page = require('./Page');
-const sharp = require('sharp');
 const fs = require('fs');
 
 Utils.removeDirectory(project.output);
-Utils.recursiveCopy(`./admin`, `${project.output}/admin`);
 Utils.recursiveCopy(`${project.src}/assets`, `${project.output}/assets`);
 
 for (const pathSchema in project.paths) {
@@ -21,8 +19,3 @@ for (const pathSchema in project.paths) {
 }
 
 fs.mkdirSync(`${project.output}/assets/images/thumbnails`);
-
-fs.readdirSync(`${project.output}/assets/images/uploads`).forEach(image => {
-  const path = `${project.output}/assets/images/uploads/${image}`;
-  sharp(path).resize(400).toFile(path.replace('uploads', 'thumbnails'))
-});
